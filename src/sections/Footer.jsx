58b1d202f92b6ec396edab00736ef1a8373b9b21
Emmanwel <mukhebiemmanuel5@gmail.com>
2023-08-28 16:46:37 +0300

    logo

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -17,8 +17,9 @@ const Footer = () => {
             />
           </a>
           <p className="mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm">
-            Get shoes ready for the new term at your nearest Nike store. Find
-            Your perfect Size In Store. Get Rewards
+            Get our products with the click of a button which are Get our
+            products with the click of a button which are...
+            Store. Get Rewards
           </p>
           <div className="flex items-center gap-5 mt-8">
             {socialMedia.map((icon) => (
